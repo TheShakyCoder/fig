878d196b0f78142fd0476dd16480dad4465f47f3
Sharif Khan <455120+TheShakyCoder@users.noreply.github.com>
2026-09-18 06:18:03 +0100

    Add Google AdSense meta tag
    Added Google AdSense meta tag for account tracking.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="google-adsense-account" content="ca-pub-3326354836489064">
+        
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
